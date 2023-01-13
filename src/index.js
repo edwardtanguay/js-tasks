4b9a17c525028e3885ac6862c3075c3dc08b622b
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-13 11:36:06 +0100

    birthdate with time

--- src/index.js
+++ src/index.js
@@ -8,10 +8,20 @@ fs.writeFileSync("src/output/employeeList.txt", employeeList);
 
 function getEmployeeList(emps) {
 	let r = '';
-	r += `There are ${emps.length} employees:`;
+	r += `There are ${emps.length} employees:\n\n`;
 	for (const emp of emps) {
-		console.log(emp.firstName);
+		const birthday = emp.birthDate;
+		r += `${emp.firstName} ${emp.lastName} - ${emp.address.country} - ${birthday}\n`;
 	}
+	return r;
+}
 
+function getEmployeeList2(emps) {
+	let r = '';
+	r += `There are ${emps.length} employees:\n\n`;
+	for (let i = 0; i < emps.length - 1; i++) {
+		const emp = emps[i];
+		r += `${emp.firstName} ${emp.lastName}\n`;
+	}
 	return r;
 }
